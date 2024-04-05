17d453c3c1047e3702638dcd1635956c86a81597
hkl <hkl@local>
2024-04-05 16:23:10 +0500

    Minor changes because of grumbling linter

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,6 +7,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.List;
 
 public class Main {
+    static final String USER_ADDED_STRING = "User с именем – %s добавлен в базу данных\n";
     public static void main(String[] args) {
         User man;
 
@@ -15,19 +16,19 @@ public class Main {
 
         man = new User("Ivan", "Ivanov", (byte) 11);
         userService.saveUser(man.getName(), man.getLastName(), man.getAge());
-        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+        System.out.printf(USER_ADDED_STRING, man.getName());
 
         man = new User("Petr", "Petrov", (byte) 22);
         userService.saveUser(man.getName(), man.getLastName(), man.getAge());
-        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+        System.out.printf(USER_ADDED_STRING, man.getName());
 
         man = new User("Sidor", "Sidorov", (byte) 33);
         userService.saveUser(man.getName(), man.getLastName(), man.getAge());
-        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+        System.out.printf(USER_ADDED_STRING, man.getName());
 
         man = new User("Незнайка", "Незнанский", (byte) 44);
         userService.saveUser(man.getName(), man.getLastName(), man.getAge());
-        System.out.printf("User с именем – %s добавлен в базу данных\n", man.getName());
+        System.out.printf(USER_ADDED_STRING, man.getName());
 
         List<User> userList = userService.getAllUsers();
 

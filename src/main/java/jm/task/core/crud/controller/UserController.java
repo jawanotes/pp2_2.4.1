1f92b6d6600c3e2af8f5b9538648ceea66017877
hkl <hkl@local>
2024-04-24 13:11:40 +0500

    controller

---
+++ src/main/java/jm/task/core/crud/controller/UserController.java
@@ -0,0 +1,52 @@
+package jm.task.core.crud.controller;
+
+import jm.task.core.crud.model.User;
+import jm.task.core.crud.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+    private static final String REDIRECT_USERS = "redirect:/users";
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String listUsers(ModelMap model) {
+        model.addAttribute("userlist", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/edit")
+    public String editPage(@RequestParam(value = "id", required = true) Long id, ModelMap model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "edit";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping("/addnew")
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.createUser(user);
+        return REDIRECT_USERS;
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return REDIRECT_USERS;
+    }
+
+    @PostMapping("/delete")
+    public String deleteUser(@ModelAttribute("user") User user) {
+        userService.removeUser(user);
+        return REDIRECT_USERS;
+    }
+}

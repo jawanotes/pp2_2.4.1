7974565de2d0dd68fe217c45d4c84a9a5a420833
hkl <hkl@local>
2024-04-24 15:02:37 +0500

    web

---
+++ src/main/webapp/index.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Users CRUD-management system</title>
+    <meta charSet="UTF-8"/>
+</head>
+<body>
+<h2>Users CRUD-management system</h2>
+<br/>
+<a href="/users">List of users</a>
+</body>
+</html>
